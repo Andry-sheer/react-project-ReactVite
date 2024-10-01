84669ce595a2adef67861390a98abe6f73ee2098
Andrew <Andry.Sheremetew@gmail.com>
2024-10-01 16:50:48 +0300

    fix h1 dashProfile => Profile

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -8,7 +8,7 @@ const DashProfile = () => {
 
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
-      <h1 className="my-7 text-center font-semibold text-3xl">DashProfile</h1>
+      <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
       <form className="flex flex-col gap-4">
         <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
           <img src={currentUser.profilePicture} alt="user" className="rounded-full w-full h-full object-cover border-8 border-[lightGray]" />
