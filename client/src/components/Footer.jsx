0bb32f85ee642b4b7c5f43e220f300f4abf9c00c
Andrew <Andry.Sheremetew@gmail.com>
2024-10-01 21:48:04 +0300

    complete upload user image function

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import {BsFacebook, BsInstagram, BsTwitter, BsGithub, BsDribbble  } from 'react-
 
 const FooterCom = () => {
   return (
-    <Footer container className="border border-t-8 border-emerald-500">
+    <Footer container className="border border-t-8 border-purple-800">
       <div className="w-full max-w-7xl mx-auto">
         <div className="grid w-full justify-between sm:flex sm:grid-cols-1">
           <div className="mt-5">
